68e15e9748d1be3a61ac50d14cdb3deb97a837b0
kappabull <kappa8v11@gmail.com>
2018-04-16 17:47:37 +0900

    グラフィック設定変更、表示コントロール用のマネージャースクリプトを追加

---
+++ wallbg/Assets/JSControlManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSControlManager : MonoBehaviour {
+	private JSControlManager _instance;
+	private void Awake() {
+		_instance = this;
+	}
+	// Use this for initialization
+	private void Start () {
+		
+	}
+}
