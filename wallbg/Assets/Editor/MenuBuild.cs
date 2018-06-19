9dcddb15c485a922e6ae795d68e14c106a1aea29
kappabull <kappa8v11@gmail.com>
2018-06-19 16:43:47 +0900

    一旦変更点追加

--- wallbg/Assets/Editor/MenuBuild.cs
+++ wallbg/Assets/Editor/MenuBuild.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,6 +21,18 @@ public class MenuBuild {
 		FileUtil.DeleteFileOrDirectory(webGLexportPath + "/index.html");
 		Directory.Delete(fixPath, true);
 		Directory.Move(webGLexportPath + "/Build/", fixPath);
+
+		// string loadjs = string.Empty;
+
+		// System.IO.StreamReader sr = new System.IO.StreamReader(webGLexportPath + "UnityLoader.js", System.Text.Encoding.GetEncoding("utf-8"));
+		// loadjs = sr.ReadToEnd();
+		// sr.Close();
+
+		// loadjs.Replace("UnityLoader.SystemInfo.hasWebGL?UnityLoader.SystemInfo.mobile?e.popup(\"Please note that Unity WebGL is not currently supported on mobiles. Press OK if you wish to continue anyway.\",[{text:\"OK\",callback:t}]):[\"Firefox\",\"Chrome\",\"Safari\"].indexOf(UnityLoader.SystemInfo.browser)==-1?e.popup(\"Please note that your browser is not currently supported for this Unity WebGL content. Press OK if you wish to continue anyway.\",[{text:\"OK\",callback:t}]):t():e.popup(\"Your browser does not support WebGL\",[{text:\"OK\",callback:r}])", "t();");
+
+		// System.IO.StreamWriter sw = new System.IO.StreamWriter(webGLexportPath + "UnityLoader.js", false, System.Text.Encoding.GetEncoding("utf-8"));
+		// sw.Write(loadjs);
+		// sw.Close();
 	}
 
 	public static string[] GetScenes() {
